4b17177534790185c1e30e2d28b5f547f83437b0
a.bakhtiari <bakhtiari.amir@gmail.com>
2022-07-08 17:36:10 +0430

    Updates

--- src/Components/Control/Image.js
+++
@@ -1,7 +0,0 @@
-const Image = (props) => {
-    let width = props.width ? props.width : 25;
-    let height = props.height ? props.height : 25;    
-    return <img style={{width:width,height:height}}  className="resume-image" src={require('/public/images/'+props.url)}  />;
-}
-
-export default Image;
